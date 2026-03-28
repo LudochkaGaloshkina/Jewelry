039c84d34a2f9ed5e08d6aad3321d384e7bf8832
LudochkaGaloshkina <akula070307@gmail.com>
2026-03-28 19:15:21 +0300

    feat: добавление post запроса на регистрацию и использвание bcrypt для хеширования пароля

--- server/server.js
+++ server/server.js
@@ -28,6 +28,43 @@ const db = await mysql.createPool({
     connectionLimit: 10
 })
 
+<<<<<<< Updated upstream
+=======
+/* ---------- REGISTER ---------- */
+
+app.post("/register", async (req,res)=>{
+    try{
+        const {name,email,password} = req.body
+
+        if(!name || !email || !password){
+            return res.json({status:"error",message:"missing fields"})
+        }
+
+        const [existing] = await db.execute(
+            "SELECT id FROM users WHERE email=?",
+            [email]
+        )
+
+        if(existing.length>0){
+            return res.json({status:"error",message:"email exists"})
+        }
+
+        const hash = await bcrypt.hash(password,10)
+
+        await db.execute(
+            `INSERT INTO users (name,email,password,role,createdAt)
+             VALUES (?,?,?,'user',NOW())`,
+            [name,email,hash]
+        )
+
+        res.json({status:"ok"})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({status:"error"})
+    }
+})
+>>>>>>> Stashed changes
 
 app.listen(3000, ()=>{
     console.log("http://localhost:3000")
